6c037ea89f93fc5f2aac8d5a37b044fb6e7ab95b
Mariia Kryvokin <kruvokin@gmail.com>
2020-02-22 18:11:57 +0200

    spring cloud config externalized

---
+++ roomservice/src/test/java/com/kryvokin/roomservice/RoomserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryvokin.roomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
